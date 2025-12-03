be81068b09b9dbd8a5640fc48a858576684a6270
suyog-jadhav <jadhavsuyog113@gmail.com>
2025-12-03 19:25:29 +0530

    user and video models added

--- src/index.js
+++ src/index.js
@@ -8,7 +8,14 @@ dotenv.config({
 })
 
 connectDB()
-
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`server is running at port ${process.env.PORT}`)
+    })
+})
+.catch((error)=>{
+    console.log("MONGODB connection failed!!!",error)
+})
 
 
 
---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,9 @@
+class ApiResponse {
+    constructor(message="Success", data, statusCode) {
+        this.success = statusCode < 400;
+        this.message = message;
+        this.data = data;
+        this.statusCode = statusCode;
+
+    }
+}
